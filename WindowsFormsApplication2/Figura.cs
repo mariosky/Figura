6bff129c532b715490134103c3b935dff3d290af
Mario Garcia Valdez <mariosky@gmail.com>
2016-04-08 10:34:10 -0700

    Primera Version

---
+++ WindowsFormsApplication2/Figura.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication2
+{
+    class Figura
+    {
+    }
+
+    class Rectangulo
+    {
+        public int X;
+        public int Y;
+        Pen pluma;
+        public int ancho;
+        public int largo;
+        Color color;
+
+        public Rectangulo(int x, int y ) {
+            X = x;
+            Y = y;
+            pluma = new Pen(Color.Black, 2);
+            ancho = 10;
+            largo = 10;
+        }
+
+        public void Draw(Form f)
+        {
+            Graphics g = f.CreateGraphics();
+            g.DrawRectangle(pluma, this.X, this.Y, ancho, largo);
+        }
+
+    }
+
+
+}
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        private List<Rectangulo> rectangulos;
+        public Form1()
+        {
+            rectangulos = new List<Rectangulo>();
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+          Rectangulo r =  new Rectangulo(e.X, e.Y);
+            r.Draw(this);
+            rectangulos.Add(r);
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Rectangulo r in rectangulos)
+                r.Draw(this);
+        }
+    }
+}
